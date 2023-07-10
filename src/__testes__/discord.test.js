906cfcc96c3d7257a08d5652f0322f6ddd754b7d
Luiz Fernando <luizfernandosv85@gmail.com>
2023-07-10 14:27:34 -0300

    Melhoria + implementação casos de teste do discord.js

--- src/__testes__/discord.test.js
+++ src/__testes__/discord.test.js
@@ -2,22 +2,51 @@ import { getWarmupTime, getTeamInfo } from '../lib/getWarmup_getTeam';
 import { JSDOM } from 'jsdom';
 
 describe('Chamando funções do discord', () => {
-    it('chama a função getWarmupTime', () => {
+    it('chama a função getWarmupTime - 1', () => {
+      /*
+      primeira versão do teste
       const warmupexpires = 10; // Exemplo: 10 segundos
       const result = getWarmupTime(warmupexpires);
-
       expect(result).toMatch(/^Até: \d{2}:\d{2}:\d{2}/); // Verifica se o resultado está no formato esperado, ignorando o restante da string
+    */
+      const warmupexpires = 10; // Exemplo: 10 segundos
+      const result = getWarmupTime(warmupexpires);
+      expect(result).toMatch(/^Até: \d{2}:\d{2}:\d{2}/); // Verifica se o resultado está no formato esperado, ignorando o restante da string
+      
     });
-    it('chama a função getTeamInfo', () => {
+    it('chama a função getWarmupTime - 2', () => {
+      const warmupexpires = -10; // Exemplo: 10 segundos
+      const result = getWarmupTime(warmupexpires);
+      if (warmupexpires<=0){
+        expect(result).toBe('Acabou!'); // Verifica se o resultado está no formato esperado, ignorando o restante da string
+      }else{
+      expect(result).toMatch(/^Até: \d{2}:\d{2}:\d{2}/); // Verifica se o resultado está no formato esperado, ignorando o restante da string
+      }
+    });
+
+    it('chama a função getTeamInfo - dados de level e nick enviados', () => {
       const membersFull = {
         data: {
-          players: [{ level: 'teste-level', nick: 'teste-nick' }]
+          players: [{ level: 'teste-level', nick: 'teste-nick'}]
         }
       };
 
       const result1 = getTeamInfo(membersFull.data);
 
       expect(result1).toBe('teste-level - teste-nick \n');
+    });
+
+    it('chama a função getTeamInfo - dados vazios', () => {
+      const membersFull = {
+        data: {
+          players: [{ level: '', nick: ''}]
+        }
+      };
+
+      const result1 = getTeamInfo(membersFull.data);
+
+      expect(result1).toBe('- \n');
+
     });
 
   });
