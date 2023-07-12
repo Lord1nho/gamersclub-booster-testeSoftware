c5551426cffcb159f270a2b1e57d4964a94a4300
Luiz Fernando <luizfernandosv85@gmail.com>
2023-07-12 00:46:07 -0300

    Upgrade no teste do loadJson

--- src/__testes__/blockList.test.js
+++ src/__testes__/blockList.test.js
@@ -31,4 +31,35 @@ describe('Chamando funções de blocklist', () => {
     expect(importOrig.value).toEqual('');
     expect(callback).toHaveBeenCalledWith('teste');
   });
+
+  it('chama a função loadJson, porém verifica o comportamento caso não haja um componente imporOrig', () => {
+    const dom = new JSDOM('<html><body></body></html>');
+
+    global.document = dom.window.document;
+    /*
+    const importOrig = document.createElement('input');
+    importOrig.id = 'importOrig';
+    document.body.appendChild(importOrig);
+    */
+   const importOrig = '';
+
+    // Mock FileReader
+    class MockFileReader {
+      readAsText(file) {
+        this.result = JSON.stringify(file.contents); // Convert to JSON string
+        this.onload();
+      }
+    }
+    global.FileReader = MockFileReader;
+
+    const callback = jest.fn();
+    const e = {
+      target: {
+        files: [{ name: 'teste.json', contents: 'teste' }]
+      }
+    };
+    loadJson(e, callback);
+    expect(importOrig.value).toEqual('');
+    expect(callback).toHaveBeenCalledWith('teste');
+  });
 });
