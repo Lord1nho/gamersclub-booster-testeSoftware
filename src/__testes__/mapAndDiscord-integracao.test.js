fd5c3481f7126603f0b4ee22052e0f6cfb07c24e
Luiz Fernando <luizfernandosv85@gmail.com>
2023-08-20 17:16:17 -0300

    Modificando teste de integração para todos os mapas

--- src/__testes__/mapAndDiscord-integracao.test.js
+++ src/__testes__/mapAndDiscord-integracao.test.js
@@ -4,55 +4,71 @@ import { getMapImage } from "../lib/maps";
 
 //Foi criado o objeto de teste gcMatch que simula os dados de uma partida.
 
+const testCases = [
+  { mapa: 'de_mirage'},
+  { mapa: 'de_dust2'},
+  { mapa: 'de_nuke'},
+  { mapa: 'de_vertigo'},
+  { mapa: 'de_train'},
+  { mapa: 'de_overpass'},
+  { mapa: 'de_inferno'},
+  { mapa: 'de_cbble_classic'},
+  { mapa: 'de_ancient'},
+  { mapa: 'de_tuscan'},
+  { mapa: 'de_anubis'},
+  { mapa: 'de_cache'},
+];
+
 describe('Teste de integração', () => {
-  test('integração sendMatchInfo com getMapImage - Mirage', async () => {
-    const gcMatch = {
-      teamA: {
-        admin: {
-          nick: 'AdminA'
+    test.each(testCases)('integração sendMatchInfo com getMapImage - %s', async (testCase) => {
+      const gcMatch = {
+        teamA: {
+          admin: {
+            nick: 'AdminA'
+          },
+          averageLevel: 5,
+          players: [
+            { level: 5, nick: "Player1"},
+            { level: 5, nick: "Player2"},
+            { level: 4, nick: "Player3"}
+          ]
         },
-        averageLevel: 5,
-        players: [
-          { level: 5, nick: "Player1"},
-          { level: 5, nick: "Player2"},
-          { level: 4, nick: "Player3"}
-        ]
-      },
-      teamB: {
-        admin: {
-          nick: 'AdminB'
+        teamB: {
+          admin: {
+            nick: 'AdminB'
+          },
+          averageLevel: 4,
+          players: [
+            { level: 4, nick: "Player4"},
+            { level: 3, nick: "Player5"},
+            { level: 3, nick: "Player6"}
+          ]
         },
-        averageLevel: 4,
-        players: [
-          { level: 4, nick: "Player4"},
-          { level: 3, nick: "Player5"},
-          { level: 3, nick: "Player6"}
-        ]
-      },
-      ip: '127.0.0.1',
-      password: 'matchpass',
-      map: {
-        name: 'de_mirage'
-      },
-      warmupExpiresInSeconds: 60,
-      gameId: '12345'
-    };
+        ip: '127.0.0.1',
+        password: 'matchpass',
+        map: {
+          name: testCase.mapa
+        },
+        warmupExpiresInSeconds: 60,
+        gameId: '12345'
+      };
+  
+      // Mock da função getMapImage
+      const originalGetMapImage = getMapImage(gcMatch.map);
+  
+      // Mock da função sendMatchInfo
+      const originalSend = global.send;
+      global.send = (url, data) => {
+        mockSend(url, data);
+      };
+  
+      await sendMatchInfo('oi', gcMatch);
+  
+      expect(originalGetMapImage).toHaveBeenCalledWith(testCase.mapa);
+  
+      // Restaura as funções originais
+      getMapImage = originalGetMapImage;
+      global.send = originalSend;
+    });
 
-    // Mock da função getMapImage
-    const originalGetMapImage = getMapImage(gcMatch.map);
-
-    // Mock da função sendMatchInfo
-    const originalSend = global.send;
-    global.send = (url, data) => {
-      mockSend(url, data);
-    };
-
-    await sendMatchInfo('oi', gcMatch);
-
-    expect(originalGetMapImage).toHaveBeenCalledWith('de_mirage');
-    
-    // Restaura as funções originais
-    getMapImage = originalGetMapImage;
-    global.send = originalSend;
-  });
 });
