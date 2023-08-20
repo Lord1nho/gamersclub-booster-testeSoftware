f29caa456a72c3c0efdfa60db05fddc25669c278
Luiz Fernando <luizfernandosv85@gmail.com>
2023-08-20 18:31:03 -0300

    Modificando expect para verificar url do getMapImage

--- src/__testes__/mapAndDiscord-integracao.test.js
+++ src/__testes__/mapAndDiscord-integracao.test.js
@@ -5,18 +5,18 @@ import { getMapImage } from "../lib/maps";
 //Foi criado o objeto de teste gcMatch que simula os dados de uma partida.
 
 const testCases = [
-  { mapa: 'de_mirage'},
-  { mapa: 'de_dust2'},
-  { mapa: 'de_nuke'},
-  { mapa: 'de_vertigo'},
-  { mapa: 'de_train'},
-  { mapa: 'de_overpass'},
-  { mapa: 'de_inferno'},
-  { mapa: 'de_cbble_classic'},
-  { mapa: 'de_ancient'},
-  { mapa: 'de_tuscan'},
-  { mapa: 'de_anubis'},
-  { mapa: 'de_cache'},
+    { mapa: 'de_mirage', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661554784/0F9E7C44F50C692C1EEC1FC677CFDD3EF646F205/' },
+    { mapa: 'de_dust2', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661556277/AD44C5610063755E8F01C666BD355903233BD71A/' },
+    { mapa: 'de_nuke', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661558001/9A12A18E071449DC606DBE0567BB73A1B152E627/' },
+    { mapa: 'de_vertigo', expectedURL: 'https://www.esportelandia.com.br/wp-content/uploads/2020/01/vertigo-posicoes-0-cke.jpg?ezimgfmt=ng:webp/ngcb2'},
+    { mapa: 'de_train', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661557181/6914D9DE513C94C61DE93F771B84087CD3A71855/'},
+    { mapa: 'de_overpass', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661555590/09F33020BF5CD1C9621BF24913667609AA24A39B/'},
+    { mapa: 'de_inferno', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661553917/B39374DE893CD45BD9361ECF82794445F3D3C6D7/'},
+    { mapa: 'de_cbble_classic', expectedURL: 'https://www.esportelandia.com.br/wp-content/uploads/2020/01/posi%C3%A7%C3%B5es-csgo-cobblestone.jpg?ezimgfmt=ng:webp/ngcb2'}, //!este caso irá apontar falha pois a url está errada
+    { mapa: 'de_ancient', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269530633050395/22CAA5E07D68FB8FDA8643DDF69A58B2A388C9F6/'},
+    { mapa: 'de_tuscan', expectedURL: 'https://external-preview.redd.it/vozvExNxbuYFYBP8b8Fu03mTH2y1ZJEzajAFtYTN1rA.jpg?auto=webp&s=88361088e82d5868ac26ecb522f009dd1c9bec0b'},
+    { mapa: 'de_anubis', expectedURL: 'https://pbs.twimg.com/media/Fh9DINkWAAAy8sw?format=jpg&name=medium'},
+    { mapa: 'de_cache', expectedURL: 'https://steamuserimages-a.akamaihd.net/ugc/1822269383661548991/2C886B2A6EAB6E266B5E84BB1039E701205A48C1/'}
 ];
 
 describe('Teste de integração', () => {
@@ -54,7 +54,6 @@ describe('Teste de integração', () => {
     };
 
     // Mock da função getMapImage
-    const originalGetMapImage = getMapImage(gcMatch.map);
 
     // Mock da função sendMatchInfo
     const mockSend = (url, data) =>{
@@ -64,11 +63,11 @@ describe('Teste de integração', () => {
     global.send = mockSend;
 
     await sendMatchInfo('url_mock', gcMatch);
+    const originalGetMapImage = getMapImage(gcMatch.map.name);
 
-    expect(originalGetMapImage).toHaveBeenCalledWith(testCase.mapa);
+    expect(originalGetMapImage).toEqual(testCase.expectedURL);
     
     // Restaura as funções originais
-    getMapImage = originalGetMapImage;
     global.send = originalSend;
   });
 });
