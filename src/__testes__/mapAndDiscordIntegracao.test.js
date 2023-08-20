ebdfb699caeb9078999edef6856d3d841885a394
jcjean <jeancharlessjesus@gmail.com>
2023-08-19 22:11:37 -0300

    Resolvido falha no Send e atualizaçaõ nos objetos da gcMatch

--- src/__testes__/mapAndDiscordIntegracao.test.js
+++ src/__testes__/mapAndDiscordIntegracao.test.js
@@ -1,26 +1,33 @@
 //Testando a função sendMatchInfo em conjunto com a função getMapImage.
-import jest from "jest";
 import {sendMatchInfo} from "../lib/getWarmup_getTeam";
 import { getMapImage } from "../lib/maps";
 
 //Foi criado o objeto de teste gcMatch que simula os dados de uma partida.
 
-
-
-describe('TEste de integração', () => {
+describe('Teste de integração', () => {
   test('integração sendMatchInfo com getMapImage', async () => {
     const gcMatch = {
       teamA: {
         admin: {
           nick: 'AdminA'
         },
-        averageLevel: 5
+        averageLevel: 5,
+        players: [
+          { level: 5, nick: "Player1"},
+          { level: 5, nick: "Player2"},
+          { level: 4, nick: "Player3"}
+        ]
       },
       teamB: {
         admin: {
           nick: 'AdminB'
         },
-        averageLevel: 4
+        averageLevel: 4,
+        players: [
+          { level: 4, nick: "Player4"},
+          { level: 3, nick: "Player5"},
+          { level: 3, nick: "Player6"}
+        ]
       },
       ip: '127.0.0.1',
       password: 'matchpass',
@@ -31,20 +38,20 @@ describe('TEste de integração', () => {
       gameId: '12345'
     };
 
-    // Mock da função getMapImage         //nao to entendendoo pq ta dando erro aquiii. O objeto pro gcMatch ta certinho. Só se for algo na hora de pegar essas urls dos mapas
+    // Mock da função getMapImage
     const originalGetMapImage = getMapImage(gcMatch.map);
 
     // Mock da função sendMatchInfo
-    const originalSend = send;
-    send = jest.fn((url, data) => {
+    const originalSend = global.send;
+    global.send = (url, data) => {
       mockSend(url, data);
-    });
+    };
 
     await sendMatchInfo('oi', gcMatch);
 
     expect(originalGetMapImage).toHaveBeenCalledWith('de_mirage');
     
-    expect(send).toHaveBeenCalledWith('mocked-url', expect.objectContaining({
+    expect(global.send).toHaveBeenCalledWith('oi', expect.objectContaining({
       image: {
         url: originalGetMapImage
     }
@@ -52,6 +59,6 @@ describe('TEste de integração', () => {
 
     // Restaura as funções originais
     getMapImage = originalGetMapImage;
-    send = originalSend;
+    global.send = originalSend;
   });
 });
