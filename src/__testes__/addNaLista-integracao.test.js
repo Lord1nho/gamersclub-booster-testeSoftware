12ed005a8999c790ab6f89837e0ac11c913cdada
jcjean <jeancharlessjesus@gmail.com>
2023-08-21 03:39:49 -0300

    Feita algumas alterações mas ainda com falha.

--- src/__testes__/addNaLista-integracao.test.js
+++ src/__testes__/addNaLista-integracao.test.js
@@ -1,30 +1,31 @@
 import { adicionarNaLista } from "../lib/blockList";
-
+//!!!!! PROVAVELMENTE POR ELA USAR ESSA API DE 'CHROME STORAGE' É UMA MERDA FAZER TESTE NELA E DA ESSES ERROS NO MOCK. Então pode excluir se quiser
 describe('Testes da função adicionarNaLista', () => {
-  it('deve adicionar um objeto à lista', (done) => {
-    const mockStorage = {
-      blockList: [],
-      get: (keys, callback) => callback(mockStorage),
-      set: (data, callback) => {
-        mockStorage.blockList = data.blockList;
+  it('deve adicionar um objeto à lista', () => {
+    const mockGet = (keys, callback) => callback({ blockList: [] });
+    const mockSet = (data, callback) => {
+      if (typeof callback === 'function') {
         callback();
-      },
+      }
     };
 
     global.chrome = {
       storage: {
-        sync: mockStorage,
+        sync: {
+          get: mockGet,
+          set: mockSet,
+        },
       },
     };
 
     const obj = { id: 1 };
 
-    adicionarNaLista(obj, () => {
-      expect(mockStorage.set).toHaveBeenCalledWith(
-        { blockList: [obj] },
-        expect.any(Function)
-      );
-      done();
-    });
+    adicionarNaLista(obj, () => {});
+
+    expect(mockGet).toHaveBeenCalledWith(['blockList'], expect.any(Function));
+    expect(mockSet).toHaveBeenCalledWith(
+      { blockList: [obj] },
+      expect.any(Function)
+    );
   });
 });
